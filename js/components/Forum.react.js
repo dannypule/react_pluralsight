647936ed36fc846a5faba4895d7b2073541c6a17
DannyP <kojikusan@gmail.com>
2016-03-15 21:08:58 +0000

    convert some of the js to jsx

--- js/components/Forum.react.js
+++ js/components/Forum.react.js
@@ -1,11 +1,32 @@
 var Forum = React.createClass({
-	
+
+    getInitialState: function(){
+
+        return {
+            allAnswers: {
+                "1": {
+                    body: "React is awesome.",
+                    correct: false
+                },
+                "2": {
+                    body: "React and flux rock.",
+                    correct: false
+                },
+                "3": {
+                    body: "React is pretty cool.",
+                    correct: false
+                },
+            }
+        }
+
+    },
+
 	render: function(){
 
 		return React.createElement(
-			'div', 
+			'div',
 			null,
-			React.createElement(ForumHeader, null)
+			React.createElement(ForumHeader, { allAnswers: this.state.allAnswers })
 		);
 
 	}
--- js/components/ForumHeader.react.js
+++ js/components/ForumHeader.react.js
@@ -1,30 +1,17 @@
 var ForumHeader = React.createClass({
+
 	render: function() {
-		return 	React.createElement(
-					'nav',
-					{
-						className: 'navbar navbar-default'
-					},
-					React.createElement(
-						'div',
-						{
-							className: 'container-fluid'
-						},
-						React.createElement(
-							'div',
-							{
-								className: 'navbar-header'
-							},
-							React.createElement(
-								'a',
-								{	
-									className: 'navbar-brand',
-									href: '#'
-								},
-								'React Forum'
-							)
-						)
-					)
-				)
+		console.log(this.props.allAnswers);
+		return 	(
+			<nav className="navbar navbar-default ">
+				<div className="container-fluid">
+					<div className="navbar-header">
+						<a href="#" className="navbar-brand">
+							React forum
+						</a>
+					</div>
+				</div>
+			</nav>
+		);
 	}
 });
