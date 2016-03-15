e517656e6439ef2490791cb13c652626effd6507
Danny Pule <danny.pule@mippin.com>
2016-03-15 17:34:18 +0000

    added forum header

--- js/components/Forum.react.js
+++ js/components/Forum.react.js
@@ -2,7 +2,11 @@ var Forum = React.createClass({
 	
 	render: function(){
 
-		return React.createElement('div', null);
+		return React.createElement(
+			'div', 
+			null,
+			React.createElement(ForumHeader, null)
+		);
 
 	}
 
